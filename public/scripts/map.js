689a5c24049685e36a172d0a3042c907e3380b83
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-11-14 15:05:57 -0800

    bout time to start displaying markers

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,6 +1,6 @@
 // Google Maps
 
-let initAutocomplete = (lat, lng, zoom) => {
+let renderMap = (lat,lng, zoom) => {
   let map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: lat, lng: lng},
     zoom: zoom,
@@ -11,9 +11,20 @@ let initAutocomplete = (lat, lng, zoom) => {
     }
   })
 
-  google.maps.event.addDomListener(window, 'resize', () => {
-    var center = map.getCenter();
-    google.maps.even.trigger(map, 'resize');
-    map.setCenter(center)
-  })
+  // google.maps.event.addDomListener(window, 'resize', () => {
+  //   var center = map.getCenter();
+  //   google.maps.even.trigger(map, 'resize');
+  //   map.setCenter(center)
+  // })
+
+  $.get(`/api/v1/${lat}${lng}`
+    .then(results => {
+      results.forEach((coordpair) => {
+        new google.maps.Marker({
+          postion: coordpair,
+          map: map
+        })
+      });
+    })
+    .err(err => console.log()`Error Code Mundy 1: ${err}`)
 };
