8bcf982cb0454751de4b0c38c6d7b9424406b259
Joe Waine <joe.waine@gmail.com>
2017-11-14 15:06:16 -0800

    get coordinates request

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
-
+const fetch = require('node-fetch');
+const fetchJson = require('node-fetch-json');
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
@@ -16,6 +17,37 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.sendFile('index.html')
 });
+
+
+
+app.get('/api/v1/markers/*', (req, res) => {
+res.send(fetchJson(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=gas+stations&location=${req.params.currentlocation}&radius=40&key=AIzaSyB6PFCWQQvDJQhVpPL6PqM3W7ZcywGO1KU`)
+  .then(response => {
+  response.results.map(function(x){
+  return { lat: x.geometry.location.lat, lng: x.geometry.location.lng };
+  }));
+  }))
+});
+
+app.get('/api/v1/markers/*', (req, res) => {
+    res.send(fetchJson(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=gas+stations&location=${req.params[0]}&radius=40&key=AIzaSyB6PFCWQQvDJQhVpPL6PqM3W7ZcywGO1KU`)
+    .then(response => {
+    response.results.map(function(x){
+    return { lat: x.geometry.location.lat, lng: x.geometry.location.lng };
+    }));
+    }))
+})
+
+
+
+
+// fetchJson('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=47.6060443,-122.3239966&destinations=47.63585519999999,-122.3756461&key=AIzaSyB6PFCWQQvDJQhVpPL6PqM3W7ZcywGO1KU')
+// .then(response => console.log(JSON.stringify(response)));
+
+
+
+
+
 
 // UNIX-Socket for connections
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
