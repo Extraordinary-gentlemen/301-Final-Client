7d59185dfe1ed8bc713a1830a11d2456d7bbc54b
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-11-15 12:00:03 -0800

    fixing whatever went nuts a min ago

--- public/scripts/controller/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Set the page.js base if site is deployed on gh-pages
-// if(location.hostname === 'but-yeah-book-list.github.io') page.base('/book-list-client');
-
-// This is where we define the client-side routes
-// page();
