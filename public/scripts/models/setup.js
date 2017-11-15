acbea36d5b1b75ed3d7b1160f3e182cd8a7285a9
Robert Reed <robert.mc.reed@gmail.com>
2017-11-15 00:08:44 -0800

    mpg auto populating upon third request. Clearing out at each other stage

--- public/scripts/models/setup.js
+++ public/scripts/models/setup.js
@@ -4,14 +4,14 @@ var app = app || {};
 
 (function(module) {
   // Set the __API_URL__ for requests to the server
-  let __API_URL__ = 'https://extraordinary-gentlemen.herokuapp.com'; // eslint-disable-line
-  if(location.hostname !== 'pumpfinder.herokuapp.com') __API_URL__ = 'http://localhost:4000';
+  // let __API_URL__ = 'https://extraordinary-gentlemen.herokuapp.com'; // eslint-disable-line
+  // if(location.hostname !== 'pumpfinder.herokuapp.com') __API_URL__ = 'http://localhost:4000';
 
   // Establishing the setup object
   const setup = {};
   setup.myCar = {};
 
-
+  // Convert returned xml data into an array of strings
   setup.parseXML = xml => {
     let data = module.xmlToJson(xml).menuItems.menuItem;
     if(!Array.isArray(data)) data = [data];
@@ -19,7 +19,7 @@ var app = app || {};
   };
 
   // Get a list of all available vehicle makes and parse as an array
-  setup.getMakes = year => { // eslint-disable-line
+  setup.getMakes = year => {
     $.get(`https://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year=${year}`)
       .then(results => {
         module.setupView.loadMakes(setup.parseXML(results));
@@ -27,7 +27,7 @@ var app = app || {};
   };
 
   // Get a list of all available vehicle models for given year and parse as an array
-  setup.getModels = (year, make) => { // eslint-disable-line
+  setup.getModels = (year, make) => {
     $.get(`https://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=${year}&make=${make}`)
       .then(results => {
         module.setupView.loadModels(setup.parseXML(results));
@@ -35,7 +35,7 @@ var app = app || {};
   };
 
   // Get the details for the specific car
-  setup.getCar = (year, make, model) => { // eslint-disable-line
+  setup.getCar = (year, make, model) => {
     $.get(`https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=${year}&make=${make}&model=${model}`)
       .then(results => {
         // TODO: Some cars return multiple options. Do we want to take that into account? Currently ignored.
@@ -45,24 +45,20 @@ var app = app || {};
 
   // Get the mpg for the specific car
   setup.getMPG = id => { // eslint-disable-line
-    console.log(`https://www.fueleconomy.gov/ws/rest/vehicle/${id}`);
     $.get(`https://www.fueleconomy.gov/ws/rest/vehicle/${id}`)
       .then(results => {
         // TODO: Some cars return multiple options. Do we want to take that into account? Currently ignored.
-        console.log(module.xmlToJson(results));
-        let carData = module.xmlToJson(results);
-        let cityMpg = Number(carData.vehicle.city08['#text']);
-        let hwyMpg = Number(carData.vehicle.highway08['#text']);
+        let carData = module.xmlToJson(results).vehicle;
+        let cityMpg = Number(carData.city08['#text']);
+        let hwyMpg = Number(carData.highway08['#text']);
         setup.myCar.mpg = {
           city: cityMpg,
           hwy: hwyMpg,
           avg: (cityMpg + hwyMpg) / 2
         };
-        console.log(setup.myCar);
+        module.setupView.$mpgInput.val(setup.myCar.mpg.avg);
       }, console.error)
   };
-
-
 
   // Test route to check communication with the API
   // $.get(`${__API_URL__}/test`)
@@ -70,3 +66,11 @@ var app = app || {};
 
   module.setup = setup;
 })(app);
+
+
+// TODO: jquery hide and show the selects
+// TODO: modify inputs so min number is 0 allow floats (see S.O.)
+// TODO: Look into fancier jQuery for showing the pieces, like slide?
+// TODO: Add some sort of recursive callback for refetching on fail, but increment (max 5 tries)
+// TODO: event handler on form submit
+// TODO: throw in a little page.js action
--- public/scripts/views/setup-view.js
+++ public/scripts/views/setup-view.js
@@ -8,8 +8,8 @@ var app = app || {};
   const $yearSelect = $('select[name="vehicle-year"]');
   setupView.$makeSelect = $('select[name="vehicle-make"]');
   setupView.$modelSelect = $('select[name="vehicle-model"]');
+  setupView.$mpgInput = $('input[name="vehicle-mpg"]');
 
-  // Select Box Methods
   setupView.loadYears = () => {
     for(let i = 2018; i > 1983; i--) {
       $yearSelect.append(`<option value="${i}">${i}</option>`);
@@ -21,20 +21,22 @@ var app = app || {};
     for(let make of makes) {
       setupView.$makeSelect.append(`<option value="${make}">${make}</option>`)
     }
-    setupView.$makeSelect.append(`<option value="none">Make Not Listed</option>`);
+    // setupView.$makeSelect.append(`<option value="none">Make Not Listed</option>`);
+    setupView.$makeSelect.show();
   };
 
   setupView.loadModels = models => {
     for(let model of models) {
       setupView.$modelSelect.append(`<option value="${model}">${model}</option>`)
     }
-    setupView.$modelSelect.append(`<option value="none">Model Not Listed</option>`);
+    // setupView.$modelSelect.append(`<option value="none">Model Not Listed</option>`);
+    setupView.$modelSelect.show();
   };
 
   setupView.emptySelect = select => {
     let first = select.children().first();
-    select.empty().append(first);
-    console.log('emptying');
+    let second = first.next();
+    select.empty().append(first).append(second);
   };
 
 
@@ -47,10 +49,10 @@ var app = app || {};
       let val = e.target.value;
       setupView.emptySelect(setupView.$makeSelect);
       setupView.emptySelect(setupView.$modelSelect);
+      setupView.$mpgInput.val('');
       if(val && val !== 'none') { // Year Selected
         module.setup.myCar.year = val;
         module.setup.getMakes(val);
-        console.log(`Nabbed ${val} makes.`);
       } else { // "Year not listed" or "Select Year" selected
         delete module.setup.myCar.year;
         if(val) { // "Year Not listed" selected
@@ -62,10 +64,10 @@ var app = app || {};
     setupView.$makeSelect.on('change', e => {
       let val = e.target.value;
       setupView.emptySelect(setupView.$modelSelect);
+      setupView.$mpgInput.val('');
       if(val && val !== 'none') { // Model Selected
         module.setup.myCar.make = val;
         module.setup.getModels(module.setup.myCar.year, val);
-        console.log(`Nabbed ${module.setup.myCar.year} ${val} models`);
       } else { // "Model not listed" or "select model" selected
         delete module.setup.myCar.make;
         if(val) { // "Model not listed" selected
@@ -76,10 +78,10 @@ var app = app || {};
 
     setupView.$modelSelect.on('change', e => {
       let val = e.target.value;
+      setupView.$mpgInput.val('');
       if(val && val !== 'none') { // Model Selected
         module.setup.myCar.model = val;
         module.setup.getCar(module.setup.myCar.year, module.setup.myCar.make, val);
-        console.log(`Nabbed ${module.setup.myCar.year} ${module.setup.myCar.make} ${val}`);
       } else { // "Model not listed" or "select model" selected
         delete module.setup.myCar.model;
         if(val) { // "Model not listed" selected
