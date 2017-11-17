5de16aa8367cc7af1b180e499ed98502775ddc34
Robert Reed <robert.mc.reed@gmail.com>
2017-11-16 19:34:17 -0800

    added font color to map info boxes

--- public/scripts/views/page-view.js
+++ public/scripts/views/page-view.js
@@ -4,7 +4,7 @@ var app = app || {};
 
 (function (module){
   const pageView = {};
-  pageView.initMapView = function(ctx){
+  pageView.initMapView = function(){
     let $result = $('#result-display');
     $('.container').hide();
     $result.removeClass('hide');
@@ -12,13 +12,13 @@ var app = app || {};
     window.google.maps.event.trigger(app.map, 'resize');
     app.map.setCenter({lat: app.lat, lng: app.lng});
   };
-  pageView.initSetupView = function(ctx){
+  pageView.initSetupView = function(){
     let $setup = $('#vehicle-setup');
     $('.container').hide();
     $setup.removeClass('hide');
     $setup.show();
   };
-  pageView.initAboutView = function(ctx){
+  pageView.initAboutView = function(){
     let $about = $('#about-us');
     $('.container').hide();
     $about.removeClass('hide');
